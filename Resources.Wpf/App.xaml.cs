64ba4d58ff2e17ab89f446a1bfbc21bd308347d9
Daniel Cazzulino <daniel@cazzulino.com>
2024-11-07 09:46:41 -0300

    Add project files.

---
+++ Resources.Wpf/App.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Globalization;
+using System.Windows;
+
+namespace Resources.Wpf
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+            CultureInfo.CurrentUICulture = CultureInfo.CurrentCulture = new("es-AR");
+        }
+    }
+}
---
+++ Resources.Wpf/MainWindow.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+
+namespace Resources.Wpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            label.Content = Strings.Milei;
+        }
+    }
+}
